8f8d629019b57f0fba4342d10e834e39d2d26ab6
Luffy-Montoya <80363512+Luffy-Montoya@users.noreply.github.com>
2025-05-21 13:28:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -23,42 +23,52 @@ async function getData() {
     const results = data.Search
     resultsSection.innerHTML = ""
 
-    results.map(async function(obj){
-        const filmRes = await fetch(`https://www.omdbapi.com/?i=${obj.imdbID}&apikey=c67d68ae`)
-        const filmData = await filmRes.json()
-        
-        resultsSection.innerHTML += `
-            <div class="result-container">
-                <img id="poster" class="movie-poster" src="${filmData.Poster}" alt="Not Found" 
-                    onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s'">
-                <div class="movie-description">
-                    <div class="result-header">
-                        <a href="https://www.imdb.com/title/${obj.imdbID}/" class="title" target="_blank">${filmData.Title}</a>
-                        <div class="rating-container">
-                            <i class="star fa-solid fa-star"></i>
-                            <div class="rating">${filmData.imdbRating}</div>
+    if (results) {
+
+        results.map(async function(obj){
+            const filmRes = await fetch(`https://www.omdbapi.com/?i=${obj.imdbID}&apikey=c67d68ae`)
+            const filmData = await filmRes.json()
+
+            resultsSection.innerHTML += `
+                <div class="result-container">
+                    <img id="poster" class="movie-poster" src="${filmData.Poster}" alt="Not Found" 
+                        onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&s'">
+                    <div class="movie-description">
+                        <div class="result-header">
+                            <a href="https://www.imdb.com/title/${obj.imdbID}/" class="title" target="_blank">${filmData.Title}</a>
+                            <div class="rating-container">
+                                <i class="star fa-solid fa-star"></i>
+                                <div class="rating">${filmData.imdbRating}</div>
+                            </div>
                         </div>
+                        <div class="second-row">
+                            <div class="year-time">    
+                                <div class="year">${filmData.Year}</div>
+                                <div class="runtime">${filmData.Runtime}</div>
+                            </div>
+                            <div class="add-container">
+                                <i id="${obj.imdbID}" class="add-to-watchlist fa-solid fa-plus"></i>
+                                <p id="${obj.imdbID}-watch" class="watchlist-label">Watchlist</p>
+                            </div>
+                        </div>
+                        <div class="genres">${filmData.Genre}</div>
+                        <div class="plot">${filmData.Plot}</div>
                     </div>
-                    <div class="second-row">
-                        <div class="year-time">    
-                            <div class="year">${filmData.Year}</div>
-                            <div class="runtime">${filmData.Runtime}</div>
-                        </div>
-                        <div class="add-container">
-                            <i id="${obj.imdbID}" class="add-to-watchlist fa-solid fa-plus"></i>
-                            <p id="${obj.imdbID}-watch" class="watchlist-label">Watchlist</p>
-                        </div>
-                    </div>
-                    <div class="genres">${filmData.Genre}</div>
-                    <div class="plot">${filmData.Plot}</div>
                 </div>
-            </div>
-            `
-        if (watchList.includes(`${obj.imdbID}`)){
-            document.getElementById(`${obj.imdbID}`).classList.add("added")
-            document.getElementById(`${obj.imdbID}-watch`).innerText = "Added"
-        }
-})}
+                `
+            if (watchList.includes(`${obj.imdbID}`)){
+                document.getElementById(`${obj.imdbID}`).classList.add("added")
+                document.getElementById(`${obj.imdbID}-watch`).innerText = "Added"
+            }
+        }) 
+    } else {
+            document.getElementById("empty-section").innerHTML = `
+        <div class="empty-container">
+            <div class="empty">Ah dang... No results.</div>
+            <div class="empty">Try searching for something else!</a>
+        </div>`
+        }      
+}
 
 function addRemove(e){
     if (e.target.className === "add-to-watchlist fa-solid fa-plus"){
